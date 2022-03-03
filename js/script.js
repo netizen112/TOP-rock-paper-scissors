b6371a4d349125c312e2efb47588bc646c334af5
netizen112 <netizen112@gmail.com>
2022-03-03 19:25:19 +0200

    Add game function

--- js/script.js
+++ js/script.js
@@ -17,23 +17,25 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = (playerSelection.charAt(0).toUpperCase()) + playerSelection.slice(1);
+    playerSelection = playerSelection.toLowerCase();
     let outcome;
 
     switch (playerSelection) {
-        case 'Rock':
+        case 'rock':
             outcome = (computerSelection == 'Paper') ? 'Lose' : (computerSelection == 'Scissors') ? 'Win' : 'Tie';
             break;
-        case 'Paper':
+        case 'paper':
             outcome = (computerSelection == 'Scissors') ? 'Lose' : (computerSelection == 'Rock') ? 'Win' : 'Tie';
             break;
-        case 'Scissors':
+        case 'scissors':
             outcome = (computerSelection == 'Rock') ? 'Lose' : (computerSelection == 'Paper') ? 'Win' : 'Tie';
             break;
-        default: 'Invalid Input';
+        default: 
+            outcome ='Invalid Input';
+            break;
     }
 
-    switch (outcome) {
+    /*switch (outcome) {
         case 'Win':
             return `You Win! ${playerSelection} beats ${computerSelection}`;
         case 'Lose':
@@ -43,5 +45,60 @@ function playRound(playerSelection, computerSelection){
         default:
             return 'Invalid Player Input';
 
+    }*/
+
+    return outcome;
+}
+
+/*const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));*/
+
+function game(){
+    let playerScore = 0
+    let computerScore = 0
+
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round ${i+1}:`);
+
+        let playerSelection = prompt(`Enter Rock, Paper or Scissors!`);
+        let computerSelection = computerPlay();
+
+        let outcome = playRound(playerSelection, computerSelection);
+
+        switch (outcome) {
+            case 'Win':
+                console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+                playerScore++;
+                break;
+            case 'Lose':
+                console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+                computerScore++;
+                break;
+            case 'Tie':
+                console.log(`It's a Tie - Player and Computer both chose ${computerSelection}`);
+                break;
+            case 'Invalid Input':
+                console.error('Invalid input');
+                i--;
+                break;
+        }
+
+        console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}`);
+    }
+
+    switch(true) {
+        case (playerScore == computerScore):
+            console.log("Game over! It's a tie.")
+            break;
+        case (playerScore > computerScore):
+            console.log("Game over - Player Wins!");
+            break;
+        case (computerScore > playerScore):
+            console.log("Game over - Computer Wins!");
+            break;
+        default:
+            console.error();
+            break;
     }
 }
